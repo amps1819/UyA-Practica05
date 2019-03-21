f0f1cab15f34bae9a11817dc1dba147def6fbd2e
amps1819 <47330054+amps1819@users.noreply.github.com>
2019-03-21 18:48:44 +0000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,20 +1,10 @@
+// 3er ejercicio
 function conversor(tipo, cantidad) {
     divisas={dolar:1.14, libra:0.87, yen:125.98};
 
     document.getElementById("outputEuros").innerHTML = cantidad/divisas[tipo];
 }
-
-function contar(){
-    document.getElementById("cantidadPs").innerHTML = document.getElementsByTagName("p").length;
-    document.getElementById("cantidadAs").innerHTML = document.getElementsByTagName("a").length;
-    document.getElementById("cantidadULs").innerHTML = document.getElementsByTagName("ul").length;
-}
-function reset(){
-    document.getElementById("cantidadPs").innerHTML = null;
-    document.getElementById("cantidadAs").innerHTML = null;
-    document.getElementById("cantidadULs").innerHTML = null;
-}
-
+// 4º ejercicio
 function tpv(){
     precios={leche:1.25, yogures:0.74, lentejas:1.49, cebollas:1.25, melocotones:1.79};
     var temp = 0;
@@ -26,4 +16,15 @@ function tpv(){
     temp += precios['melocotones'] * document.getElementById("inputMelocotones").value;
     temp += (0.065 * temp); // 6,5% IGIC general (desde el 01/01/2019)
     document.getElementById("outputCuenta").innerHTML = temp+" euros";
+}
+// 5º ejercicio
+function contar(){
+    document.getElementById("cantidadPs").innerHTML = document.getElementsByTagName("p").length;
+    document.getElementById("cantidadAs").innerHTML = document.getElementsByTagName("a").length;
+    document.getElementById("cantidadULs").innerHTML = document.getElementsByTagName("ul").length;
+}
+function reset(){
+    document.getElementById("cantidadPs").innerHTML = null;
+    document.getElementById("cantidadAs").innerHTML = null;
+    document.getElementById("cantidadULs").innerHTML = null;
 }
