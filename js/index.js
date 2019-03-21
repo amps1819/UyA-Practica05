745795bc079acac8e42592fb1c3d7b007851f86d
amps1819 <47330054+amps1819@users.noreply.github.com>
2019-03-21 19:01:38 +0000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,28 @@
-// 3er ejercicio
+function reset(){
+
+    document.getElementById("inputDolares").value = null;
+    document.getElementById("inputLibras").value = null;
+    document.getElementById("inputYenes").value = null;
+    document.getElementById("outputEuros").innerHTML = null;
+
+    document.getElementById("inputLeche").value = null;
+    document.getElementById("inputYogures").value = null;
+    document.getElementById("inputLentejas").value = null;
+    document.getElementById("inputCebollas").value = null;
+    document.getElementById("inputMelocotones").value = null;
+    document.getElementById("outputCuenta").innerHTML = null;
+
+    document.getElementById("cantidadPs").innerHTML = null;
+    document.getElementById("cantidadAs").innerHTML = null;
+    document.getElementById("cantidadULs").innerHTML = null;
+}
+
 function conversor(tipo, cantidad) {
     divisas={dolar:1.14, libra:0.87, yen:125.98};
 
     document.getElementById("outputEuros").innerHTML = cantidad/divisas[tipo];
 }
-// 4º ejercicio
+
 function tpv(){
     precios={leche:1.25, yogures:0.74, lentejas:1.49, cebollas:1.25, melocotones:1.79};
     var temp = 0;
@@ -17,14 +35,9 @@ function tpv(){
     temp += (0.065 * temp); // 6,5% IGIC general (desde el 01/01/2019)
     document.getElementById("outputCuenta").innerHTML = temp+" euros";
 }
-// 5º ejercicio
+
 function contar(){
     document.getElementById("cantidadPs").innerHTML = document.getElementsByTagName("p").length;
     document.getElementById("cantidadAs").innerHTML = document.getElementsByTagName("a").length;
     document.getElementById("cantidadULs").innerHTML = document.getElementsByTagName("ul").length;
-}
-function reset(){
-    document.getElementById("cantidadPs").innerHTML = null;
-    document.getElementById("cantidadAs").innerHTML = null;
-    document.getElementById("cantidadULs").innerHTML = null;
 }
