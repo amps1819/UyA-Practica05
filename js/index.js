6690e1fb038ad7be722a9d6ca3c543b9e4d7a2f3
amps1819 <47330054+amps1819@users.noreply.github.com>
2019-03-21 09:57:40 +0000

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,20 @@
+function toEuros(cantidad, tipo) {
+    if (tipo === "D")
+    {
+    	document.getElementById("outputEuros").innerHTML=cantidad/1.14;
+    }
+    else if (tipo === "L")
+    {
+    	document.getElementById("outputEuros").innerHTML=cantidad/0.87;
+    }
+    else
+    {
+    	document.getElementById("outputEuros").innerHTML=cantidad/125.98;
+    }
+}
+
+function contar(){
+    document.getElementById("cantidadPs").innerHTML = document.getElementsByTagName("p").length;
+    document.getElementById("cantidadAs").innerHTML = document.getElementsByTagName("a").length;
+    document.getElementById("cantidadULs").innerHTML = document.getElementsByTagName("ul").length;
+}
